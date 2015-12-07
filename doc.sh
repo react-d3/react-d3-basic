5691fe8002eae79d91cee51f8ebc4bf873841c30
JackHou <vjack070707@gmail.com>
2015-12-07 22:49:29 +0800

    move docs_pre to docs

--- doc.sh
+++ doc.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-for filename in docs_pre/**/*.md docs_pre/**/**/*.md; do
-    dist=${filename//docs_pre\//docs\/}
+
+# remove old docs
+for dir in docs/*; do
+    if test $dir != docs/docs_pre 
+    then
+        rm -rf $dir
+    fi
+done
+# compile new
+for filename in docs/docs_pre/**/*.md docs/docs_pre/**/**/*.md; do
+    dist=${filename//docs\/docs_pre\//docs\/}
     dir=${dist%/*}
     mkdir -p "$dir"
     smashmd $filename > $dist
